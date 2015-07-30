1949fa5b8bc0aaaedc19dc84e4a9f0ab9c7bea53
Soeren <soeren@siegismund-poschmann.de>
2015-07-30 19:14:36 +0200

    Mail Dump

--- server.py
+++ server.py
@@ -22,10 +22,10 @@ def handle(to, sender, subject, body):
             os.makedirs("./mails")
         if not os.path.exists("./mails/%s" % sender):
             os.makedirs("./mails/%s" % sender)
-                d = datetime.now()
-                fp = open("./mails/%s/%s_%s.txt" % (sender, d.strftime("%y_%m_%d"), subject))
-                fp.write(body)
-                fp.close()
+        d = datetime.datetime.now()
+        fp = open("./mails/%s/%s_%s.txt" % (sender, d.strftime("%y_%m_%d"), subject), "w")
+        fp.write(body)
+        fp.close()
     con = mdb.connect(config.get('MYSQL', 'host'), config.get('MYSQL', 'user'), config.get('MYSQL', 'pass'), config.get('MYSQL', 'db'));
     cur = con.cursor()
     cur.execute("SELECT * FROM `mailChain` ORDER BY `mailChain`.`prio` ASC")
